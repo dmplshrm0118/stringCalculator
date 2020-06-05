e1cc0db43b146a014042ca80df4772e94dc1cf1f
dmplshrm0118 <66403035+dmplshrm0118@users.noreply.github.com>
2020-06-05 22:33:32 +0530

    Commit 2

--- AddStringNumbers.java
+++ AddStringNumbers.java
@@ -1,11 +1,5 @@
 package Assessment;
-import java.lang.Exception;
-
-class NegativeException extends Exception{
-    NegativeException(String s){
-        super(s);
-    }
-}
+import java.util.*;
 
 public class AddStringNumbers {
     
@@ -18,6 +12,9 @@ public class AddStringNumbers {
                 //declaring & Initalizing sum variable
                 int sum = 0;
                 
+                //Declaring Negative Value ArrayList
+                ArrayList negatives =new ArrayList();
+                
                 // Replacing "\n" with ","
                 num = num.replaceAll("\n",",");
                 
@@ -25,7 +22,7 @@ public class AddStringNumbers {
                 if(num.charAt(0) == '/' && num.charAt(1) == '/'){
                     char ch = num.charAt(2);
                     num = num.replace(ch, ',');
-                    num = num.replace('/', ',');
+                    num = num.replaceAll("[^a-zA-Z0-9]", ",");
                 }
                 // Converting String Into Number Array By using split method
                 String[] numbers = num.split(",");
@@ -34,16 +31,28 @@ public class AddStringNumbers {
                     if(numbers[i].isEmpty()){
                         numbers[i] = "0";
                     }
+                    int number =Integer.parseInt(numbers[i]);
+                    //Checking If Number Is Negative
+                    if(number < 0){
+                        negatives.add(number);
+                    }
+                    //Checking If Number Is Greater Than 1000
+                    if(number >= 1000){
+                        number = 0;
+                    }
                     // Else Doing the Sum Of All Numbers In the String And Returning To Main
-                    sum += Integer.parseInt(numbers[i]);
+                    sum += number;
                 }
+                if(negatives.size() > 0)
+                    throw new IllegalArgumentException("Negatives Not Allowed"+negatives);
+                else
                 return sum;
             } 
     }
     public static void main(String[] args) {
-        String a = "6,\n";
+        String input = "//[*][%]\n1*2%3";
         // Calling Add Function With String Input
-        int result = Add(a);
+        int result = Add(input);
         System.out.println("Sum Of String: "+result);   
     }
 }
